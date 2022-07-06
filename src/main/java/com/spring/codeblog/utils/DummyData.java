006bd5be509f5d363084fbba85e720cf7a3b95a9
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-07-05 22:01:25 -0300

    adicionando método POST

--- src/main/java/com/spring/codeblog/utils/DummyData.java
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -21,21 +21,18 @@ public class DummyData {
 
 		List<Post> postList = new ArrayList<>();
 		Post post1 = new Post();
-		post1.setAutor("Vitor Santos");
+		post1.setAutor("Layza Gabriela");
 		post1.setData(LocalDate.now());
-		post1.setTitulo("Docker");
+		post1.setTitulo("O Acompanhante Terapêutico na Psicologia");
 		post1.setTexto(
-				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-
-		Post post2 = new Post();
-		post2.setAutor("Layza Gabriela");
-		post2.setData(LocalDate.now());
-		post2.setTitulo("API REST");
-		post2.setTexto(
-				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+				"O acompanhamento terapêutico como trabalho surgiu na Argentina na década de 1960, como uma alternativa às práticas terapêuticas clássicas. Essa atividade foi endossada pelos movimentos antimanicomiais que ganharam força na Europa nas décadas de 1950 e 1960 (Londero & Pacheco, 2006), e que tinham como objetivo reestruturar a atenção à saúde mental (diminuindo o papel da internação psiquiátrica como principal forma de intervenção, e focando em procedimentos que visassem à integração do paciente à comunidade)."
+				+ "Considerando as premissas da Reforma Psiquiátrica, o acompanhante terapêutico apareceu como uma forma de promover a inclusão social, e prevenir a piora gradual da doença, diminuindo a autonomia do paciente (processo conhecido como cronificação) e a institucionalização (a permanência continuada em instituições de saúde mental) (Carniel & Pedrão, 2010). No Brasil, o início desse tipo de atuação começou nas comunidades terapêuticas, nas quais os ATs - geralmente jovens universitários do curso de enfermagem - promoviam atividades cotidianas e recreativas, e posteriormente, com a diminuição das comunidades terapêuticas, passaram a atuar no contexto residencial dos pacientes, entrando em contato com o cotidiano e universos familiares (Londero & Pacheco, 2006)."
+				+ "A definição de Acompanhante Terapêutico encontra diversas propostas na literatura da saúde mental e da psicologia clínica. Dentro da Análise do Comportamento, o AT é definido “ora como o profissional que trabalha no ambiente onde as contingências mantenedoras dos comportamentos a serem alterados operam, ora como o auxiliar de um terapeuta comportamental ou de um psiquiatra ou, ainda, de uma equipe multidisciplinar” (Guerrelas, 2007, p. 34). Ou seja, o AT pode ser:"
+				+ "o profissional cuja atuação está diretamente e unicamente relacionada com o contexto natural do cliente (em casa, no trabalho, na escola, etc);"
+				+ "uma parte de um processo clínico que é baseado em consultório, como na obtenção de dados por meio de observação direta a serem utilizados na terapia em consultório;"
+				+ "uma extensão da intervenção.");
 
 		postList.add(post1);
-		postList.add(post2);
 
 		for (Post post : postList) {
 			Post postSaved = CodeblogRepository.save(post);
