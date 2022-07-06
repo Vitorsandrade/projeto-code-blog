690e9750c63d99ff4fa444810773df81e2d1b5bd
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-07-06 14:19:38 -0300

    summernote

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -47,7 +47,7 @@ public class CodeblogController {
 	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
 	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attribues) {
 		if (result.hasErrors()) {
-			attribues.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos.");
+			attribues.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
 			return "redirect:/newpost";
 		}
 		post.setData(LocalDate.now());
